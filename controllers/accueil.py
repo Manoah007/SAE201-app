887a4ecdc849d5ebb90dda25b8fe079278465cbb
b_malik <malik.boussaidi@etu.u-pec.fr>
2026-06-05 16:00:21 +0200

    Ajout de la page Api Ameli

--- controllers/accueil.py
+++ controllers/accueil.py
@@ -16,3 +16,23 @@ def index():
                                 professions=professions)
     finally:
         session.close()
+
+@bp_accueil.route("/api-ameli")
+def api_ameli():
+    """Page API Ameli : formulaire de consultation des effectifs."""
+    session = Session()
+    try:
+        regions = session.query(Region).order_by(Region.libelle).all()
+        professions = (
+            session.query(ProfessionSante)
+            .order_by(ProfessionSante.libelle)
+            .all()
+        )
+
+        return render_template(
+            "api_ameli.html",
+            regions=regions,
+            professions=professions
+        )
+    finally:
+        session.close()
