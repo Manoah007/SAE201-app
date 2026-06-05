2daae39bf8b76b82a07d36de710fc5251ba9bd60
b_malik <malik.boussaidi@etu.u-pec.fr>
2026-06-05 15:15:03 +0200

    Ajout du fichier erreur.html et modifications app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from config import Config
 from controllers.accueil import bp_accueil
 from controllers.api import bp_api
 from controllers.effectifs import bp_effectifs
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -20,6 +21,20 @@ def inject_base_url():
 app.register_blueprint(bp_accueil)
 app.register_blueprint(bp_api)
 app.register_blueprint(bp_effectifs)
+
+@app.errorhandler(404)
+def page_non_trouvee(e):
+    return render_template(
+        "erreur.html",
+        message="Page non trouvée."
+    ), 404
+
+@app.errorhandler(500)
+def erreur_serveur(e):
+    return render_template(
+        "erreur.html",
+        message="Erreur interne. Réessayez plus tard."
+    ), 500
 
 if __name__ == "__main__":
  app.run(debug=True)
