e53d19ad214fa60ab80a7d0bea92223263a55537
a_dhali <afnanmasud.dhali@etu.u-pec.fr>
2026-06-17 21:47:44 +0200

    Modification de la route Flask pour le dashboard

--- controllers/accueil.py
+++ controllers/accueil.py
@@ -1,20 +1,23 @@
 from flask import Blueprint, render_template
 from models.db import Session
 from models.dimensions import Region, ProfessionSante
+from services.dashboard_service import get_dashboard_context
 
 bp_accueil = Blueprint("accueil", __name__)
 
 @bp_accueil.route("/")
 @bp_accueil.route("/accueil")
 def index():
-    """Page d'accueil : affiche les régions et professions."""
+    """Page d'accueil : affiche les régions, professions et le dashboard Ameli."""
     session = Session()
     try:
         regions = session.query(Region).order_by(Region.libelle).all()
         professions = (session.query(ProfessionSante).order_by(ProfessionSante.libelle).all())
+        contexte_dashboard = get_dashboard_context()
         return render_template("accueil.html",
                                 regions=regions,
-                                professions=professions)
+                                professions=professions,
+                                **contexte_dashboard)
     finally:
         session.close()
 
