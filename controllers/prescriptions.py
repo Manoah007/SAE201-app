893eb7c9b7caa4ea7e8134e7c979603f0fee87a6
Manoah_Gilles <manoah.gilles@etu.u-pec.fr>
2026-06-21 17:08:57 +0200

    Répartition du contenue des pages HTML

--- controllers/prescriptions.py
+++ controllers/prescriptions.py
@@ -20,31 +20,31 @@ def page_disparite():
     resultats = None
 
     try:
-        # Chargement des données depuis la BD pour alimenter les listes déroualntes
+        # Récupère les tables pour charger listes déroulantes
         regions = session.query(Region).order_by(Region.libelle).all()
         departements = session.query(Departement).order_by(Departement.libelle).all()
 
         
         # Récupération des choix de l'utilisateur (via l'URL en GET)
         # Renvoie l'ID de chaque choix pour les identifiés 
-        region_ids = request.args.getlist("region_id") # On récupère une liste d'ID pour des choix mutliple
-        departement_ids = request.args.getlist("departement_id")
+        region_list_id = request.args.getlist("region_id") # On récupère une liste d'ID pour des choix mutliple
+        departement_list_id = request.args.getlist("departement_id")
         annee = request.args.get("annee", type=int)
 
 
-        if not region_ids and not departement_ids and not annee:
+        if not region_list_id and not departement_list_id and not annee:
             resultats = api.get_prescription_default()
 
         # Envoi de TOUTES les variables nécessaires au template Jinja2
         return render_template(
-            "prescriptions/pages_disparite/page_disparite.html",
+            "prescriptions/page_disparite.html",
             # Pour charger les listes
             regions=regions,
             departements=departements,
             #Pour filtrer les données
             annee=annee,
-            region_ids=region_ids,
-            departement_ids=departement_ids,
+            region_id=region_list_id,
+            departement_id=departement_list_id,
             resultats=resultats,
         )
     
